2c47b8094f2b9952dfb3763c438d82b0f5a173ad
dbenson24 <derek.benson@tufts.edu>
2015-10-13 14:56:44 -0400

    build scripts

---
+++ scripts/make.bat
@@ -0,0 +1,20 @@
+SET ARMA="F:\SteamLibrary\SteamApps\common\Arma 3\
+SET PBO="C:\Program Files (x86)\Bohemia Interactive\Tools\BinPBO Personal Edition\BinPBO.exe"
+
+SET ProjectHome=%~dp0..\
+
+SET ServerFiles=%ProjectHome%extDB-Build\life_server
+SET ClientFiles=%ProjectHome%extDB-Build\Altis_Life.Altis
+SET ExtDB=%ProjectHome%@extDB\
+
+echo Working in %ProjectHome%
+
+
+call %PBO% %ServerFiles%
+call %PBO% %ClientFiles%
+
+copy %ServerFiles%.pbo %ARMA%@LifeServer\addons\life_server.pbo"
+copy %ExtDB%extdb-conf.ini %ARMA%@ExtDB\extdb-conf.ini"
+copy %ExtDB%extdb\db_custom\altis-life-rpg-4.ini %ARMA%@ExtDB\extdb\db_custom\altis-life-rpg-4.ini"
+
+copy %ClientFiles%.pbo %ARMA%\mpmissions\Altis_Life.Altis.pbo"
