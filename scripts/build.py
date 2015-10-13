9cda0f0700c2cd005f981f89dc26f2c248bf7240
dbenson24 <derek.benson@tufts.edu>
2015-10-12 20:43:44 -0400

    Added build script for dialogs

--- scripts/build.py
+++ scripts/build.py
@@ -5,7 +5,13 @@ list_of_files = []
 path = "../extDB-Build/Altis_Life.Altis/dialog"
 for (dirpath, dirnames, filenames) in os.walk(path):
     for filename in filenames:
-        if (filename.endswith('.hpp') or filename.endswith('.h')) and filename != "macro.h":
+        if (filename.endswith('.hpp') or filename.endswith('.h')) and filename != "macro.h" and filename != "MasterHandler.h":
             list_of_files.append(filename)
 
 print list_of_files
+output = open(path + "/MasterHandler.h", "w")
+output.write("/*\n	Master UI Resource File\n*/\n")
+for file in list_of_files:
+    output.write("#include \"")
+    output.write(file)
+    output.write("\"\n")
